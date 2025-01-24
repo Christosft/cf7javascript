ce3201a95b45698b9a85497a7bb57ffc312b1a67
Chris Ftoulis <christosftoulis3@gmail.com>
2025-01-25 00:55:46 +0200

    javascript

--- jsproject7/intro/carsSpeed.js
+++ jsproject7/intro/carsSpeed.js
@@ -1,32 +1,59 @@
-const cars = {
-  ferrari: 350,
-  lambo: 330,
-  porsche: 310,
-  maserati: 295
+const cars = [{
+  brand: "Ferrari",
+  maxSpeed: 350
+},
+{
+  brand: "Lamborghinhi",
+  maxSpeed: 330
+},
+{
+  brand: "Porsche",
+  maxSpeed: 310
+},
+{
+  brand: "Maserati",
+  maxSpeed: 305
+}]
+
+const drivers = [{
+  lastname: prompt("Give driver's name"),
+  driverTopSpeed: 305
+},
+{
+  lastname: prompt("Give driver's name"),
+  driverTopSpeed: 330
+},
+{
+  lastname: prompt("Give driver's name"),
+  driverTopSpeed: 350
+},
+{
+  lastname: prompt("Give driver's name"),
+  driverTopSpeed: 310
+}]
+
+const speedLimit = 310;
+
+function carTest() {
+  cars.forEach(car => {
+    if (car.maxSpeed > speedLimit) {
+      document.write(`--${car.brand} top speed ${car.maxSpeed} is more than the ${speedLimit}`)
+    } else {
+      document.write(`--${car.brand} top speed ${car.maxSpeed} is less than the ${speedLimit}`)
+    }
+  });
 }
 
-const topSpeed = 310;
-
-if (cars.ferrari > topSpeed) {
-  document.write(`Ferrari top speed ${cars.ferrari} is more than 310kmh`);
-} else {
-  document.write(`Ferrari top speed ${cars.ferrari} is less than 310kmh`)
+function testDriver() {
+  drivers.forEach(driver => {
+    cars.forEach(car => {
+      if (driver.driverTopSpeed == car.maxSpeed) {
+        document.write(`--The ${driver.lastname} will be the driver of ${car.brand}`)
+      }
+    })
+  })
 }
 
-if (cars.lambo > topSpeed) {
-  document.write(`--- Lambo top speed ${cars.lambo} is more than 310kmh`);
-} else {
-  document.write(`--- Lambo top speed ${cars.lambo} is less than 310kmh`)
-}
+carTest()
+testDriver()
 
-if (cars.porsche > topSpeed) {
-  document.write(`--- Porsche top speed ${cars.porsche} is more than 310kmh`);
-} else {
-  document.write(`--- Porsche top speed ${cars.porsche} is less than 310kmh`)
-}
-
-if (cars.maserati > topSpeed) {
-  document.write(`--- Maserati top speed ${cars.maserati} is more than 310kmh`);
-} else {
-  document.write(`--- Maserati top speed ${cars.maserati} is less than 310kmh`)
-}
