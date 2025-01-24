b39558912d40147b3d03a67b74f5c7651a159382
Chris Ftoulis <christosftoulis3@gmail.com>
2025-01-24 19:09:16 +0200

    24/01/25 javascript lesson

---
+++ jsproject7/review/5e-greet.js
@@ -0,0 +1,8 @@
+
+
+function greet(username) {
+  return `Hello, ${username}`
+}
+
+console.log(greet("Andrew"));
+console.log(greet("Anna"))
---
+++ jsproject7/review/7b-arrow-greet.js
@@ -0,0 +1,8 @@
+const greet = (username) => `Hello ${username}`
+const sayHello = () => console.log("Hello!")
+const add = (a, b) => a + b
+
+console.log(greet("Christos"))
+console.log(greet("Christina"))
+sayHello();
+console.log(add(10, 5));
